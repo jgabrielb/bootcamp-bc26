d5523dba235ff2f7272ad97a4d761ee6f6a01dbc
Jorge Gabriel Benito <jorgem98gb@gmail.com>
2022-07-11 16:24:49 -0500

    using discovery server (eureka client/server)

--- ms-accounts/src/main/java/com/nttdata/msaccounts/MsAccountsApplication.java
+++ ms-accounts/src/main/java/com/nttdata/msaccounts/MsAccountsApplication.java
@@ -2,8 +2,10 @@ package com.nttdata.msaccounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsAccountsApplication {
 
     public static void main(String[] args) {
